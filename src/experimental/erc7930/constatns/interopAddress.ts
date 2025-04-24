dbc163aa8e7551a667838f3b76994e563d4333ef
0xAaCE <0xAaCE@proton.me>
2025-04-24 14:53:45 -0300

    feat: remove constant to use viem chains

--- src/experimental/erc7930/constatns/interopAddress.ts
+++ src/experimental/erc7930/constatns/interopAddress.ts
@@ -3,14 +3,3 @@ export const INTEROP_ADDRESS_VERSION = '1'
 export const CHAIN_TYPE = {
   'EIP-155': '0x0',
 } as const
-
-export const EIP_155_CHAIN_REFERENCE = {
-  mainnet: '0x1',
-  ropsten: '0x3',
-  rinkeby: '0x4',
-  goerli: '0x5',
-  sepolia: '0x6',
-  optimism: '0x7',
-  'optimism-sepolia': '0x8',
-  arbitrum: '0x7',
-} as const
